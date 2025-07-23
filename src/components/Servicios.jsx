2fb4d2415d904df01b6b3d405bd561753136d9cf
Richfullz <alejandropascualsanchez@gmail.com>
2025-07-23 22:09:36 +0200

    miporfolio

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,21 +1,47 @@
-import React from "react";
-
 export const Servicios = () => {
   return (
     <div className="page">
       <h1 className="heading">Servicios</h1>
+
       <section className="services">
-        <article>
-          <h2>Desarrollo web</h2>
-          <p>creando una pagina con una gran creatividad</p>
+        <article className="service-card">
+          <h2>Desarrollo web personalizado:</h2>
+          <p>
+            Proyectos web adaptados a las necesidades específicas de cada
+            cliente, con diseño atractivo y funcionalidad óptima.
+          </p>
         </article>
-        <article>
-          <h2>Desarrollo web</h2>
-          <p>creando una pagina con una gran creatividad</p>
+
+        <article className="service-card">
+          <h2>Aplicaciones con React:</h2>
+          <p>
+            Creación de aplicaciones SPA con interfaces interactivas y
+            rendimiento optimizado.
+          </p>
         </article>
-        <article>
-          <h2>Desarrollo web</h2>
-          <p>creando una pagina con una gran creatividad</p>
+
+        <article className="service-card">
+          <h2>Backend con Node.js:</h2>
+          <p>
+            Desarrollo de servidores robustos y escalables con Express y gestión
+            segura de bases de datos.
+          </p>
+        </article>
+
+        <article className="service-card">
+          <h2>Despliegue y hosting:</h2>
+          <p>
+            Implementación y mantenimiento en plataformas como GitHub Pages para
+            asegurar disponibilidad y rapidez.
+          </p>
+        </article>
+
+        <article className="service-card">
+          <h2>Soporte y solución de problemas:</h2>
+          <p>
+            Atención rápida a errores y mantenimiento para mantener la
+            estabilidad del sitio web.
+          </p>
         </article>
       </section>
     </div>
