0b19b69be1b5b88a9c28c1ae20b4a7b43ec2fb60
Richfullz <alejandropascualsanchez@gmail.com>
2025-07-23 22:34:05 +0200

    miporfolio

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -20,26 +20,22 @@ export const Curriculum = () => {
           </li>
           <li>
             <strong>Participación</strong> en proyectos prácticos
-            aplicandometodologías ágiles y<strong>trabajo en equipo.</strong>
+            aplicandometodologías ágiles, <strong>trabajo en equipo.</strong>
           </li>
           <li>
             <strong>Enfoque</strong> en la <strong>resolución</strong> de
             problemas y aprendizaje continuo.
           </li>
           <li>
-            <strong>implementación</strong> de soluciones Integrales:
-            Frontend,Backend y bases de datos.
+            <strong>Implementación</strong> de soluciones integrales: Frontend,
+            Backend y Bases de Datos.
           </li>
           <li>
-            <strong>Manejo de tecnologías</strong> MERN(MongoDB, Express,
-            React,Node.js)
-          </li>
-          <li>
-            Trabajo <strong>colaborativo</strong> con metodologías{" "}
-            <strong>ágiles</strong>(scrum)
+            Trabajo <strong>colaborativo</strong> con metodologías
+            <strong> ágiles</strong> (scrum)
           </li>
         </ul>
-        <h4>MERN (MongoDB, Express, React, Node.js)</h4>
+        <h4>TECNOLOGIAS: Full-Stack MERN (MongoDB, Express, React, Node.js)</h4>
         <h3>Proyectos Personales</h3>
         <div className="proyectos-personales">
           {trabajos
@@ -53,9 +49,9 @@ export const Curriculum = () => {
                   alt={proyecto.nombre}
                   className="img-proyecto-personal"
                 />
-                <h4>{proyecto.nombre}</h4>
+                <h2>{proyecto.nombre}</h2>
                 <p>{proyecto.descripcion}</p>
-                <p>Tecnologías: {proyecto.tecnologias}</p>
+                <h3>Tecnologías: {proyecto.tecnologias}</h3>
               </div>
             ))}
         </div>
@@ -65,36 +61,41 @@ export const Curriculum = () => {
         <ul>
           <h2>Desarrollo Web - Estudio 27</h2>
           <h3>2022</h3>
-          <li>Diseño de plantillas a demanda del cliente</li>
-          <li>Análisis y Posicionamiento en SEO, redactando artículos,</li>
           <li>
-            Manejo de tecnologías: Wordpress, PrestaShop, Posicionamiento en Seo
+            Diseño de plantillas <strong> a demanda del cliente</strong>
           </li>
+          <li>Análisis y Posicionamiento en SEO, redactando artículos,</li>
         </ul>
-        <h4>Wordpress, Prestashop, HTML5, CSS3, Posicionamiento en SEO</h4>
+        <h4>
+          TECNOLOGIAS: Wordpress, Prestashop, HTML5, CSS3, Posicionamiento en
+          SEO
+        </h4>
         <h3>Proyectos Personales</h3>
         <ul>
-          <h3>Visita las catedrales</h3>
+          <h2>Visita las catedrales</h2>
           <p>
             Pagina informativa sobre la Playa De Las Catedrales. <br />
             Lugo, Galicia
           </p>
-          <p>Tecnologías: Wordpress</p>
+          <h3>Tecnologías: Wordpress</h3>
         </ul>
         {/*  */}
         <hr />
         <ul>
           <h3>Desarrollo Web - ViveFormación</h3>
           <h3>2021</h3>
-          <li>560 horas de formación intensiva de desarrollo web</li>
-          <li>Construcción de paginas Web</li>
-          <li>Integración de Componentes Software en páginas Web</li>
           <li>
-            Publicación de Páginas webManejo de Tecnologías: HTML5, CSS3,
-            JavaScript
+            560 horas de <strong>formación intensiva</strong> de desarrollo web
           </li>
+          <li>
+            <strong>Construcción</strong> de paginas Web
+          </li>
+          <li>
+            <strong>Integración de Componentes</strong> Software en páginas Web
+          </li>
+          <li>Publicación de Páginas web</li>
         </ul>
-        <h4>HTML5, CSS, JavaScript, Node, Express</h4>
+        <h4>TECNOLOGIAS: HTML5, CSS, JavaScript, Node, Express</h4>
         <h3>Proyectos Personales</h3>
         <div className="proyectos-personales">
           {trabajos
@@ -108,9 +109,9 @@ export const Curriculum = () => {
                   alt={proyecto.nombre}
                   className="img-proyecto-personal"
                 />
-                <h4>{proyecto.nombre}</h4>
+                <h2>{proyecto.nombre}</h2>
                 <p>{proyecto.descripcion}</p>
-                <p>Tecnologías: {proyecto.tecnologias}</p>
+                <h3>Tecnologías: {proyecto.tecnologias}</h3>
               </div>
             ))}
         </div>
--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -7,40 +7,44 @@ export const Servicios = () => {
         <article className="service-card">
           <h2>Desarrollo web personalizado:</h2>
           <p>
-            Proyectos web adaptados a las necesidades específicas de cada
-            cliente, con diseño atractivo y funcionalidad óptima.
+            Proyectos web <strong>adaptados</strong> a las necesidades
+            específicas de cada cliente, con{" "}
+            <strong>diseño atractivo y funcionalidad óptima.</strong>
           </p>
         </article>
 
         <article className="service-card">
           <h2>Aplicaciones con React:</h2>
           <p>
-            Creación de aplicaciones SPA con interfaces interactivas y
-            rendimiento optimizado.
+            <strong>Creación de aplicaciones SPA</strong> con interfaces
+            interactivas y rendimiento optimizado.
           </p>
         </article>
 
         <article className="service-card">
           <h2>Backend con Node.js:</h2>
           <p>
-            Desarrollo de servidores robustos y escalables con Express y gestión
-            segura de bases de datos.
+            Desarrollo de <strong>servidores robustos y escalables</strong> con
+            Express y <strong>gestión segura</strong>
+            de bases de datos.
           </p>
         </article>
 
         <article className="service-card">
           <h2>Despliegue y hosting:</h2>
           <p>
-            Implementación y mantenimiento en plataformas como GitHub Pages para
-            asegurar disponibilidad y rapidez.
+            <strong>Implementación y mantenimiento</strong> en plataformas como
+            <strong>GitHub Pages</strong> para asegurar
+            <strong> disponibilidad y rapidez.</strong>
           </p>
         </article>
 
         <article className="service-card">
           <h2>Soporte y solución de problemas:</h2>
           <p>
-            Atención rápida a errores y mantenimiento para mantener la
-            estabilidad del sitio web.
+            <strong>Atención rápida a errores</strong> y mantenimiento para
+            mantener la
+            <strong> estabilidad</strong> del sitio web.
           </p>
         </article>
       </section>
