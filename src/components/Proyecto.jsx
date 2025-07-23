8da89d505aa35d2f767ae7c505500dd7d17d7e81
Richfullz <alejandropascualsanchez@gmail.com>
2025-07-23 20:19:23 +0200

    miporfolio

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -5,23 +5,29 @@ import { trabajos } from "../data/trabajos";
 export const Proyecto = () => {
   const [proyecto, setProyecto] = useState({});
   const params = useParams();
+
   useEffect(() => {
-    //filtrar trabajo por el id
-    let proyecto = trabajos.filter((trabajo) => trabajo.id === params.id);
-    //filter filtra por una condicion por cada uno de los trabajos en este caso por cada id
-    setProyecto(proyecto[0]);
-  }, []); //se ejecuta solo una vez
+    const proyectoFiltrado = trabajos.find(
+      (trabajo) => trabajo.id === params.id
+    );
+    setProyecto(proyectoFiltrado || {});
+  }, [params.id]);
 
   return (
     <div className="page page-work">
       <div className="mask">
-        <img src={"/images/" + proyecto.id + ".png"} />
+        {proyecto.id && (
+          <img
+            src={`${process.env.PUBLIC_URL}/images/${proyecto.id}.png`}
+            alt={proyecto.nombre}
+          />
+        )}
       </div>
       <div className="description">
         <h1 className="heading">{proyecto.nombre}</h1>
         <p>{proyecto.tecnologias}</p>
         <p>{proyecto.descripcion}</p>
-        <a href={proyecto.url} target="_blank">
+        <a href={proyecto.url} target="_blank" rel="noreferrer">
           Ir al proyecto
         </a>
       </div>
